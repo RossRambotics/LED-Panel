0a0239d16415ae47ff72a7155670481e4c72abe7
hollisms <info@spyschuler.com>
2022-01-19 23:58:46 -0500

    added addressable led

--- src/main/java/frc/robot/subsystems/LEDPanel/LEDPanel.java
+++ src/main/java/frc/robot/subsystems/LEDPanel/LEDPanel.java
@@ -4,11 +4,32 @@
 
 package frc.robot.subsystems.LEDPanel;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDPanel extends SubsystemBase {
+  private AddressableLED m_led = null;
+  private AddressableLEDBuffer m_ledBuffer = null;
+
   /** Creates a new LEDPanel. */
-  public LEDPanel() {}
+  public LEDPanel() {
+
+    // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(9);
+
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+
+  }
 
   @Override
   public void periodic() {
